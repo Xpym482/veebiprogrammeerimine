cd1099400490a32a98073ed3b5b53e38464dc741
Maksim Jelizarov <xpym482@users.noreply.github.com>
2018-09-10 11:36:40 +0300

    vaikese vahetused phps

--- index.php
+++ index.php
@@ -1,16 +1,53 @@
+<?php
+	//echo "Siin on PHP hren";
+	$name = "Maksim";
+	$surname = "Jelizarov";
+	$todaydate = date("d.m.Y");
+	$hournow = date("H");
+	$partofday = "";
+	if ($hournow < 8){
+	
+		$partofday = "varajane hommik";
+	}
+	if ($hournow >= 8 and $hournow < 16){
+	
+		$partofday = "kooliaeg";
+	}
+	/*if ($hournow >= 16)
+	{
+		$partofday = "vaba aeg";
+	}*/
+	else
+	{
+		$partofday = "vabaeg";
+	}
+?>
 <!DOCTYPE html>
 <html>
   <head>
     <meta charset="utf-8">
-	<title>Maksim Jelizarov, õppetöö</title>
+	<title><?php
+	echo $name;
+	echo ' ';
+	echo $surname;
+	?>, õppetöö</title>
   </head>
   <body>
-    <h1>Maksim Jelizarov</h1>
+    <h1>
+	<?php
+		echo $name ." ". $surname;
+		
+	?>
+	</h1>
 	<p>Ülikooli aadress <a href="http://www.tlu.ee" target="_blank">TLU</a>
 	</p>
+	<?php 
+		echo "<p>Tänane kuupäev on: " .$todaydate ."</p>\n";
+		echo "<p> Lehe avamise hetkel oli kell " .date('H:i:s') .", käes on " .$partofday .".</p> \n";
+	?>
 	<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜTerra õppehoone">
 	<!--../../~ringe/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg
 	<img src="/home/ringe/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TL� Terra �ppehoone"-->
-	<p>mul on sõber, kes ka teeb mingi <a href="../../~erkksul">veebi</a>.</p>
+	<p>Mul on sõber, kes ka teeb mingi <a href="../../~erkksul">veebi</a>.</p>
   </body>
 </html>
