0e0df31c34000027180a28a1b940ef90aa65cd42
Maksim Jelizarov <xpym482@users.noreply.github.com>
2018-09-10 10:40:53 +0300

    html muudetud phps

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+	<title>Maksim Jelizarov, õppetöö</title>
+  </head>
+  <body>
+    <h1>Maksim Jelizarov</h1>
+	<p>Ülikooli aadress <a href="http://www.tlu.ee" target="_blank">TLU</a>
+	</p>
+	<img src="http://greeny.cs.tlu.ee/~rinde/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TLÜTerra õppehoone">
+	<!--../../~ringe/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg
+	<img src="/home/ringe/veebiprogrammeerimine2018s/tlu_terra_600x400_1.jpg" alt="TL� Terra �ppehoone"-->
+	<p>mul on sõber, kes ka teeb mingi <a href="../../~erkksul">veebi</a>.</p>
+  </body>
+</html>
